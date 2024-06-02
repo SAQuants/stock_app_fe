e73de97de682fe8cec52ec57f17725e91a255a26
Shobhit Anirudh <Shobhit.Anirudh@gmail.com>
2024-06-02 14:10:20 +0200

    showSidebarNavigation = false

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python -m streamlit run ./stock_app_main.py --server.port 8000 --server.address 0.0.0.0
+python -m streamlit run ./stock_app_main.py --server.port 8000 --server.address 0.0.0.0 --client.showSidebarNavigation false
